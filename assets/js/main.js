89549378f96956a15720325f9d0eaa115021f7bc
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-04 17:15:48 +0100

    esercizio terminato con aggiunta di trasformazione in maiuscola della prima lettera del cognome dato da utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,23 @@
 //chiedo all'utente il suo cognome
 var cognome = prompt("qual è il tuo cognome?");
 
+//trasformo l'eventuale prima lettera minuscola in maiuscola
+cognome = cognome[0].toUpperCase() + cognome.substring(1);
+
 //creo un array con altri cognomi
 var lista_cognomi = ['Bianchi', 'Rossi', 'Perlari', 'Lodigiani', 'Mastrobattista', 'Verdi'];
-console.log(lista_cognomi);
 
 //aggiungo cognome alla lista
 lista_cognomi.push(cognome);
-console.log(lista_cognomi);
 
 //metto i cognomi in ordine alfabetico
 lista_cognomi.sort();
-console.log(lista_cognomi);
 
-//stampo una lista ordinata alfabeticamente con ciclo
+//stampo una lista ordinata alfabeticamente con ciclo for
 for (var i = 0; i < lista_cognomi.length; i++) {
-  document.getElementById('lista').innerHTML += "<li>" + lista_cognomi[i]  + "</li>";
+  document.getElementById('lista').innerHTML += "<li>" + lista_cognomi[i] + "</li>";
 }
 
 //stampo posizione "umana" della lista in cui il nuovo utente si trova
 var posizione_umana = lista_cognomi.indexOf(cognome) + 1;
-console.log(posizione_umana);
 document.getElementById('posizione').innerHTML ='Il tuo cognome è in ' + posizione_umana + '° posizione';
